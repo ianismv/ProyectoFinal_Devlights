171a583e0ca0ba7e81c9c03230ca5fad8a8acf36
Esteban Joel <119338357+Scarface974@users.noreply.github.com>
2024-10-24 13:01:16 -0300

    carga de backend

---
+++ Proyecto_DevLight_Backend/Proyecto_DevLight/Program.cs
@@ -0,0 +1,40 @@
+using Proyecto_DevLight.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Habilitar CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+builder.Services.AddSingleton<EmpleadoData>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Usar CORS
+app.UseCors("AllowAllOrigins");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
