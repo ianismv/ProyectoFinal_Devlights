972e362a061f0859f72e200603c6635425606f6e
ianismv <manosianis@gmail.com>
2024-11-19 13:36:15 -0300

    Email recibía null, se agrego a EmpleadoData.

--- ProyectoFinal_DevLight_Backend/Proyecto_DevLight/obj/Debug/net8.0/Proyecto_DevLight.AssemblyInfo.cs
+++ ProyectoFinal_DevLight_Backend/Proyecto_DevLight/obj/Debug/net8.0/Proyecto_DevLight.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_DevLight")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30765ae75fcfde142574e4820204e99df9173e55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_DevLight")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto_DevLight")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
